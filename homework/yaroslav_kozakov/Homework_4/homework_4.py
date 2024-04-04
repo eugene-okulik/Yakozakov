1a5665200190a7689aa37a65439c26761bc8d082
Yaroslav Kozakov <ykzlearn@gmail.com>
2024-04-04 21:01:58 +0300

    modified:homework/yaroslav_kozakov/Homework_4/homework_4.py

--- homework/yaroslav_kozakov/Homework_4/homework_4.py
+++ homework/yaroslav_kozakov/Homework_4/homework_4.py
@@ -1,19 +1,24 @@
-my_dict = {'tuple', 'list', 'dict', 'set'}
+my_dict = {
+    'tuple': 
+    (1, 10, 'text', 3.14, True), 
+    'list': 
+    [2, 111, 'Hello!', 'number', False], 
+    'dict': 
+    {1: 1, 'one': "biger_zero", 'two': '2', 'Love': 'love', False: True}, 
+    'set':
+    {1, 'one', 'two', 2.2, False}
+    }
 
-tuple = (1, 10, 'text', 3.14, True)
-print(tuple[-1])
+print('tuple'[-1])
 
-list = [2, 111, 'Hello!', 'number', False]
 list.append(3333)
 list.pop(1)
 print(list)
 
-dict = {1: 1, 'one': "biger_zero", 'two': '2', 'Love': 'love', False: True}
 dict['i am a tuple'] = 'six value'
 removed_value = dict.pop(1)
 print(dict)
 
-set = {1, 'one', 'two', 2.2, False}
 set.add(111)
 set.remove(1)
 print(set)
