90921a728bff6a2de80523ed66071e26c82c66e1
Yaroslav Kozakov <ykzlearn@gmail.com>
2024-04-05 22:53:32 +0300

    modified:homework/yaroslav_kozakov/Homework_4/homework_4.py

--- homework/yaroslav_kozakov/Homework_4/homework_4.py
+++ homework/yaroslav_kozakov/Homework_4/homework_4.py
@@ -1,26 +1,22 @@
 my_dict = {
-    'tuple':
-    (1, 10, 'text', 3.14, True),
-    'list':
-    [2, 111, 'Hello!', 'number', False],
-    'dict':
-    {1: 1, 'one': "biger_zero", 'two': '2', 'Love': 'love', False: True},
-    'set':
-    {1, 'one', 'two', 2.2, False}
+    'tuple':(1, 10, 'text', 3.14, True),
+    'list':[2, 111, 'Hello!', 'number', False],
+    'dict':{1: 1, 'one': "biger_zero", 'two': '2', 'Love': 'love', False: True},
+    'set':{1, 'one', 'two', 2.2, False}
 }
 
-print('tuple'[-1])
+print(my_dict['tuple'][-1])
 
-list.append(3333)
-list.pop(1)
-print(list)
+my_dict['list'].append(3333)
+my_dict['list'].pop(1)
+print(my_dict['list'])
 
-dict['i am a tuple'] = 'six value'
-removed_value = dict.pop(1)
-print(dict)
+my_dict['dict'][('i am a tuple','six')] = 'six'
+removed_value = my_dict['dict'].pop('two')
+print(my_dict['dict'])
 
-set.add(111)
-set.remove(1)
-print(set)
+my_dict['set'].add(111)
+my_dict['set'].remove(1)
+print(my_dict['set'])
 
 print(my_dict)
