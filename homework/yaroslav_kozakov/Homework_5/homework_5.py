76e914cf992972037e79a07349fea2b1fe94a351
Yaroslav Kozakov <ykzlearn@gmail.com>
2024-04-10 19:23:41 +0300

    modified:homework/yaroslav_kozakov/Homework_5/homework_5.py

--- homework/yaroslav_kozakov/Homework_5/homework_5.py
+++ homework/yaroslav_kozakov/Homework_5/homework_5.py
@@ -1,4 +1,4 @@
-# Задача1
+# # Задача1
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
 name, last_name, city, phone, country = person
 print(name, last_name, city, phone, country)
@@ -8,9 +8,13 @@ str1 = str('результат операции: 42')
 str2 = str('результат операции: 514')
 str3 = str('результат операции: 9')
 
-number_str1 = str1[19:]
-number_str2 = str2[19:]
-number_str3 = str3[19:]
+str1_colon_index = str1.index(':')
+str2_colon_index = str2.index(':')
+str3_colon_index = str3.index(':')
+
+number_str1 = str1[str1_colon_index + 2:]
+number_str2 = str2[str2_colon_index + 2:]
+number_str3 = str3[str3_colon_index + 2:]
 
 number_str1_new = int(number_str1) + 10
 number_str2_new = int(number_str2) + 10
@@ -20,7 +24,7 @@ number_result = number_str1_new + number_str2_new + number_str3_new
 
 print(number_result)
 
-# Задача3
+# # Задача3
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
 print('Students', ', ' .join(students), 'study these subjects:', ', ' .join(subjects))
