60e243a06d0b41c282794e73ad6d6fd256cbef68
Yaroslav Kozakov <ykzlearn@gmail.com>
2024-04-22 21:47:12 +0300

    modified:   homework/yaroslav_kozakov/Homework_6/homework_6_1.py & modified:   homework/yaroslav_kozakov/Homework_6/homework_6_2.py

--- homework/yaroslav_kozakov/Homework_6/homework_6_1.py
+++ homework/yaroslav_kozakov/Homework_6/homework_6_1.py
@@ -1,11 +1,13 @@
-# Напишите программу, которая добавляет ‘ing’ к словам (к каждому слову) 
-# в тексте “Etiam tincidunt neque erat, quis molestie enim imperdiet vel. 
-# Integer urna nisl, facilisis vitae semper at, dignissim vitae libero” 
-# и после этого выводит получившийся текст на экран. 
-# Знаки препинания не должны оказаться внутри слова. 
-# Если после слова идет запятая или точка, этот знак препинания должен идти после того же слова, но уже преобразованного.
+# Напишите программу, которая добавляет ‘ing’ к словам (к каждому слову)
+# в тексте “Etiam tincidunt neque erat, quis molestie enim imperdiet vel.
+# Integer urna nisl, facilisis vitae semper at, dignissim vitae libero”
+# и после этого выводит получившийся текст на экран.
+# Знаки препинания не должны оказаться внутри слова.
+# Если после слова идет запятая или точка, этот знак препинания должен идти
+# после того же слова, но уже преобразованного
 
-text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+text = ('Etiam tincidunt neque erat, quis molestie enim imperdiet vel.\
+Integer urna nisl, facilisis vitae semper at, dignissim vitae libero')
 words = text.split() # split by method split string to the list
 print(words)
 new_words =[] # create a new list where will be added w+ing words
--- homework/yaroslav_kozakov/Homework_6/homework_6_2.py
+++ homework/yaroslav_kozakov/Homework_6/homework_6_2.py
@@ -1,14 +1,14 @@
-# Напишите программу, которая перебирает последовательность от 1 до 100. 
-# Для чисел кратных 3 она должна написать: "Fuzz" вместо печати числа, а для чисел кратных 5 печатать "Buzz". 
-# Для чисел которые кратны одновременно и 3 и 5 надо печатать "FuzzBuzz". 
+# Напишите программу, которая перебирает последовательность от 1 до 100.
+# Для чисел кратных 3 она должна написать: "Fuzz" вместо печати числа, а для чисел кратных 5 печатать "Buzz".
+# Для чисел которые кратны одновременно и 3 и 5 надо печатать "FuzzBuzz".
 # Иначе печатать число.
 
 for i in range(1, 101): # iterate over the numbers in the loop for from 1 to 101 inclusive
-    if i%3 == 0 and i%5 == 0: # check divisibility up to 3 and 5 
+    if i % 3 == 0 and i % 5 == 0: # check divisibility up to 3 and 5
         print("FuzzBuzz")
-    elif i%3 == 0:      # check divisibility up to 3
+    elif i % 3 == 0: # check divisibility up to 3
         print("fuzz")
-    elif i%5 == 0:      # check divisibility up to 5
+    elif i % 5  == 0: # check divisibility up to 5
         print("buzz")
     else:
         print(i)
